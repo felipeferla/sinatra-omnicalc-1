279e4d383e0b9ea2076935f5c8c3d2bb5a84b943
felipeferla <164975883+felipeferla@users.noreply.github.com>
2024-04-16 09:33:14 +0000

    v1

--- app.rb
+++ app.rb
@@ -9,18 +9,24 @@ get("/square/results") do
   @num_to_square = params.fetch("to-square").to_i
   @square = @num_to_square ** 2
 
-  erb(:results)
+  erb(:square_results)
 end
 get("/square/new") do
 
-  erb(:results)
+  erb(:square)
+end
+
+get("/square_root/results") do
+  @num_to_square_root = params.fetch("to-square-root").to_i
+  @root = @num_to_square_root ** (1.0 / 2)
+  
+  erb(:square_root_results)
 end
 
 get("/square_root/new") do
-  @num_to_square_root = params.fetch("to-square-root").to_i
-  @root = @num_to_square_root ** (1 / 2)
 
   erb(:square_root)
+
 end
 
 get("/payment/new") do
